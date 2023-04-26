0ffb93de37cf114392698edfa3c8e8057e3905e1
manyi <1762019598@qq.com>
2023-04-27 01:01:50 +0200

    add user/room mapper tests

--- src/test/java/ch/uzh/ifi/hase/soprafs23/rest/mapper/DTOMapperTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/rest/mapper/DTOMapperTest.java
@@ -1,7 +1,61 @@
 package ch.uzh.ifi.hase.soprafs23.rest.mapper;
 
+import ch.uzh.ifi.hase.soprafs23.constant.UserStatus;
+import ch.uzh.ifi.hase.soprafs23.entity.Room;
+import ch.uzh.ifi.hase.soprafs23.entity.User;
+import ch.uzh.ifi.hase.soprafs23.rest.dto.room.RoomPostDTO;
+import ch.uzh.ifi.hase.soprafs23.rest.dto.user.UserGetDTO;
+import ch.uzh.ifi.hase.soprafs23.rest.dto.user.UserPostDTO;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DTOMapperTest {
+    @Test
+    public void testCreateUser_fromUserPostDTO_toUser_success() {
+        // create UserPostDTO
+        UserPostDTO userPostDTO = new UserPostDTO();
+        userPostDTO.setPassword("name");
+        userPostDTO.setUsername("username");
 
+        // MAP -> Create user
+        User user = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
+
+        // check content
+        // assertEquals(userPostDTO.getPassword(), user.get());
+        assertEquals(userPostDTO.getUsername(), user.getUsername());
+    }
+
+    @Test
+    public void testGetUser_fromUser_toUserGetDTO_success() {
+        // create User
+        User user = new User();
+        user.setPassword("Firstname Lastname");
+        user.setUsername("firstname@lastname");
+        user.setStatus(UserStatus.OFFLINE);
+        user.setToken("1");
+
+        // MAP -> Create UserGetDTO
+        UserGetDTO userGetDTO = DTOMapper.INSTANCE.convertEntityToUserGetDTO(user);
+
+        // check content
+        assertEquals(user.getId(), userGetDTO.getId());
+        // assertEquals(user.getName(), userGetDTO.getName());
+        assertEquals(user.getUsername(), userGetDTO.getUsername());
+        assertEquals(user.getStatus(), userGetDTO.getStatus());
+    }
+
+    @Test
+    public void room_success(){
+        RoomPostDTO room = new RoomPostDTO();
+        room.setMode(2);
+        room.setRoomName("1");
+        room.setOwnerId(1L);
+
+        Room room1 = DTOMapper.INSTANCE.convertRoomPostDTOtoEntity(room);
+
+        assertEquals(room.getRoomName(),room1.getRoomName());
+        assertEquals(room.getMode(),room1.getMode());
+        assertEquals(room.getOwnerId(),room1.getOwnerId());
+    }
 }
