5f78f6f7b44d61323d4e12396d8414c759f3d4a7
manyi <1762019598@qq.com>
2023-04-27 11:06:49 +0200

    Fix roomRepoTest error

--- src/test/java/ch/uzh/ifi/hase/soprafs23/repository/RoomRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/repository/RoomRepositoryIntegrationTest.java
@@ -2,11 +2,17 @@ package ch.uzh.ifi.hase.soprafs23.repository;
 
 
 import ch.uzh.ifi.hase.soprafs23.constant.RoomStatus;
+import ch.uzh.ifi.hase.soprafs23.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs23.entity.Room;
+import ch.uzh.ifi.hase.soprafs23.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -25,7 +31,6 @@ public class RoomRepositoryIntegrationTest {
         // given
         Room room = new Room();
         room.setMode(2);
-        room.setId(1L);
         room.setRoomName("1");
         room.setOwnerId(1L);
         room.setStatus(RoomStatus.WAITING);
@@ -50,7 +55,7 @@ public class RoomRepositoryIntegrationTest {
         // given
         Room room = new Room();
         room.setMode(2);
-        room.setId(1L);
+        //room.setId(1L);
         room.setRoomName("1");
         room.setOwnerId(1L);
         room.setStatus(RoomStatus.WAITING);
