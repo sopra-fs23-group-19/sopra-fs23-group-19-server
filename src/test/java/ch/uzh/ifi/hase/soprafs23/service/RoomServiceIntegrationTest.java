844b1e19cd52c1698f32a4389d4e96752f39fb09
manyi <1762019598@qq.com>
2023-04-27 17:22:57 +0200

    fix room service integration test

--- src/test/java/ch/uzh/ifi/hase/soprafs23/service/RoomServiceIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/service/RoomServiceIntegrationTest.java
@@ -1,7 +1,7 @@
 package ch.uzh.ifi.hase.soprafs23.service;
 
 import ch.uzh.ifi.hase.soprafs23.constant.RoomStatus;
-import ch.uzh.ifi.hase.soprafs23.constant.UserStatus;
+//import ch.uzh.ifi.hase.soprafs23.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs23.entity.Room;
 import ch.uzh.ifi.hase.soprafs23.entity.User;
 import ch.uzh.ifi.hase.soprafs23.repository.RoomRepository;
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.server.ResponseStatusException;
+//import org.springframework.web.server.ResponseStatusException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -56,7 +56,6 @@ public class RoomServiceIntegrationTest {
 
         Room newRoom = roomService.createRoom(room);
 
-        assertEquals(1L,newRoom.getId());
         assertEquals(room.getRoomName(),newRoom.getRoomName());
         assertEquals(room.getMode(),newRoom.getMode());
         assertEquals(room.getOwnerId(),newRoom.getOwnerId());
