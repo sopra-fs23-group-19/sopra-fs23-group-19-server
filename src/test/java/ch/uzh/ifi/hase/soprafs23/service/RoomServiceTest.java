9fb73f2adc6fd0a5edc019aa89160ae3ed0639de
Shiyu-Ran <shiyu.ran@uzh.ch>
2023-05-21 11:02:10 +0200

    add more tests

--- src/test/java/ch/uzh/ifi/hase/soprafs23/service/RoomServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/service/RoomServiceTest.java
@@ -13,6 +13,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RoomServiceTest {
@@ -60,121 +64,154 @@ public class RoomServiceTest {
         assertEquals(room.getStatus(),newRoom.getStatus());
     }
 
-//    @Test
-//    public void joinRoom_success(){
-//        Room room = new Room();
-//        room.setMode(2);
-//        room.setId(1L);
-//        room.setRoomName("1");
-//        room.setOwnerId(1L);
-//        room.setStatus(RoomStatus.WAITING);
-//        Mockito.when(roomRepository.findByid(Mockito.any())).thenReturn(room);
-//
-//        User user = new User();
-//        user.setPassword("Firstname Lastname");
-//        user.setUsername("firstname@lastname");
-//        user.setStatus(UserStatus.OFFLINE);
-//        user.setId(1L);
-//        user.setToken("1");
-//        List<User> users = new ArrayList<>();
-//        users.add(user);
-//        Mockito.when(userRepository.findByid(Mockito.any())).thenReturn(user);
-//        Mockito.when(userRepository.findByRoomId(Mockito.any())).thenReturn(users);
-//
-//        roomService.joinRoom(1L,1L);
-//
-//        assertEquals(1L, user.getRoomId());
-//    }
+    @Test
+    public void joinRoom_success(){
+        Room room = new Room();
+        room.setMode(2);
+        room.setId(1L);
+        room.setRoomName("1");
+        room.setOwnerId(1L);
+        room.setStatus(RoomStatus.WAITING);
+        Mockito.when(roomRepository.findByid(Mockito.any())).thenReturn(room);
 
-//    @Test
-//    public void leaveRoom_player_success(){
-//        Room room = new Room();
-//        room.setMode(2);
-//        room.setId(1L);
-//        room.setRoomName("1");
-//        room.setOwnerId(2L);
-//        room.setStatus(RoomStatus.WAITING);
-//        Mockito.when(roomRepository.findByid(Mockito.any())).thenReturn(room);
-//
-//        User user = new User();
-//        user.setPassword("Firstname Lastname");
-//        user.setUsername("firstname@lastname");
-//        user.setStatus(UserStatus.OFFLINE);
-//        user.setId(1L);
-//        user.setToken("1");
-//        user.setRoomId(1L);
-//        List<User> users = new ArrayList<>();
-//        users.add(user);
-//        Mockito.when(userRepository.findByid(Mockito.any())).thenReturn(user);
-//        Mockito.when(userRepository.findByRoomId(Mockito.any())).thenReturn(users);
-//
-//        roomService.leaveRoom(1L,1L);
-//
-//        assertEquals(null, user.getRoomId());
-//    }
+        User user = new User();
+        user.setPassword("Firstname Lastname");
+        user.setUsername("firstname@lastname");
+        user.setStatus(UserStatus.OFFLINE);
+        user.setId(1L);
+        user.setToken("1");
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        Mockito.when(userRepository.findByid(Mockito.any())).thenReturn(user);
+        Mockito.when(userRepository.findByRoomId(Mockito.any())).thenReturn(users);
 
-//    @Test
-//    public void leaveRoom_owner_success(){
-//        Room room = new Room();
-//        room.setMode(2);
-//        room.setId(1L);
-//        room.setRoomName("1");
-//        room.setOwnerId(1L);
-//        room.setStatus(RoomStatus.WAITING);
-//        Mockito.when(roomRepository.findByid(Mockito.any())).thenReturn(room);
-//
-//        User user = new User();
-//        user.setPassword("Firstname Lastname");
-//        user.setUsername("firstname@lastname");
-//        user.setStatus(UserStatus.OFFLINE);
-//        user.setId(1L);
-//        user.setToken("1");
-//        user.setRoomId(1L);
-//        List<User> users = new ArrayList<>();
-//        users.add(user);
-//        Mockito.when(userRepository.findByid(Mockito.any())).thenReturn(user);
-//        Mockito.when(userRepository.findByRoomId(Mockito.any())).thenReturn(users);
-//
-//        roomService.leaveRoom(1L,1L);
-//
-//        assertEquals(RoomStatus.END, room.getStatus());
-//    }
+        roomService.joinRoom(1L,1L);
 
-//    @Test
-//    public void retrieveRoom_success() {
-//        Room room = new Room();
-//        room.setMode(2);
-//        room.setId(1L);
-//        room.setRoomName("1");
-//        room.setOwnerId(1L);
-//        room.setStatus(RoomStatus.WAITING);
-//        Mockito.when(roomRepository.findByid(Mockito.any())).thenReturn(room);
-//
-//        Room newRoom = roomService.retrieveRoom(1L);
-//        Mockito.verify(roomRepository, Mockito.times(1)).findByid(Mockito.any());
-//
-//        assertEquals(room.getId(),newRoom.getId());
-//        assertEquals(room.getRoomName(),newRoom.getRoomName());
-//        assertEquals(room.getMode(),newRoom.getMode());
-//        assertEquals(room.getOwnerId(),newRoom.getOwnerId());
-//        assertEquals(room.getStatus(),newRoom.getStatus());
-//    }
+        assertEquals(1L, user.getRoomId());
+    }
 
-//    @Test
-//    public void availableRooms_success() {
-//        Room room = new Room();
-//        room.setMode(2);
-//        room.setId(1L);
-//        room.setRoomName("1");
-//        room.setOwnerId(1L);
-//        room.setStatus(RoomStatus.WAITING);
-//        List<Room> rooms = Collections.singletonList(room);
-//        Mockito.when(roomRepository.findAll()).thenReturn(rooms);
-//
-//        List<Room> r = roomService.getAvailableRooms();
-//        Mockito.verify(roomRepository, Mockito.times(1)).findAll();
-//
-//        assertEquals(r, rooms);
-//    }
+    @Test
+    public void leaveRoom_player_success(){
+        Room room = new Room();
+        room.setMode(2);
+        room.setId(1L);
+        room.setRoomName("1");
+        room.setOwnerId(2L);
+        room.setStatus(RoomStatus.WAITING);
+        Mockito.when(roomRepository.findByid(Mockito.any())).thenReturn(room);
+
+        User user = new User();
+        user.setPassword("Firstname Lastname");
+        user.setUsername("firstname@lastname");
+        user.setStatus(UserStatus.OFFLINE);
+        user.setId(1L);
+        user.setToken("1");
+        user.setRoomId(1L);
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        Mockito.when(userRepository.findByid(Mockito.any())).thenReturn(user);
+        Mockito.when(userRepository.findByRoomId(Mockito.any())).thenReturn(users);
+
+        roomService.leaveRoom(1L,1L);
+
+        assertEquals(null, user.getRoomId());
+    }
+
+    @Test
+    public void leaveRoom_owner_success(){
+        Room room = new Room();
+        room.setMode(2);
+        room.setId(1L);
+        room.setRoomName("1");
+        room.setOwnerId(1L);
+        room.setStatus(RoomStatus.WAITING);
+        Mockito.when(roomRepository.findByid(Mockito.any())).thenReturn(room);
+
+        User user = new User();
+        user.setPassword("Firstname Lastname");
+        user.setUsername("firstname@lastname");
+        user.setStatus(UserStatus.OFFLINE);
+        user.setId(1L);
+        user.setToken("1");
+        user.setRoomId(1L);
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        Mockito.when(userRepository.findByid(Mockito.any())).thenReturn(user);
+        Mockito.when(userRepository.findByRoomId(Mockito.any())).thenReturn(users);
+
+        roomService.leaveRoom(1L,1L);
+
+        assertEquals(RoomStatus.END, room.getStatus());
+    }
+
+    @Test
+    public void retrieveRoom_success() {
+        Room room = new Room();
+        room.setMode(2);
+        room.setId(1L);
+        room.setRoomName("1");
+        room.setOwnerId(1L);
+        room.setStatus(RoomStatus.WAITING);
+        Mockito.when(roomRepository.findByid(Mockito.any())).thenReturn(room);
+
+        Room newRoom = roomService.retrieveRoom(1L);
+        Mockito.verify(roomRepository, Mockito.times(1)).findByid(Mockito.any());
+
+        assertEquals(room.getId(),newRoom.getId());
+        assertEquals(room.getRoomName(),newRoom.getRoomName());
+        assertEquals(room.getMode(),newRoom.getMode());
+        assertEquals(room.getOwnerId(),newRoom.getOwnerId());
+        assertEquals(room.getStatus(),newRoom.getStatus());
+    }
+
+    @Test
+    public void availableRooms_success() {
+        Room room = new Room();
+        room.setMode(2);
+        room.setId(1L);
+        room.setRoomName("1");
+        room.setOwnerId(1L);
+        room.setStatus(RoomStatus.WAITING);
+        List<Room> rooms = Collections.singletonList(room);
+        Mockito.when(roomRepository.findAll()).thenReturn(rooms);
+        User user = new User();
+        user.setPassword("Firstname Lastname");
+        user.setUsername("firstname@lastname");
+        user.setStatus(UserStatus.OFFLINE);
+        user.setId(1L);
+        user.setToken("1");
+        user.setRoomId(1L);
+        List<User> users = Collections.singletonList(user);
+        Mockito.when(userRepository.findByRoomId(Mockito.anyLong())).thenReturn(users);
+
+        List<Room> r = roomService.getAvailableRooms();
+        Mockito.verify(roomRepository, Mockito.times(1)).findAll();
+
+        assertEquals(r, rooms);
+    }
+
+    @Test
+    public void getAllPlayersIds_success() {
+        Room room = new Room();
+        room.setMode(2);
+        room.setId(1L);
+        room.setRoomName("1");
+        room.setOwnerId(1L);
+        room.setStatus(RoomStatus.WAITING);
+        Mockito.when(roomRepository.findByid(Mockito.anyLong())).thenReturn(room);
+        User user = new User();
+        user.setPassword("Firstname Lastname");
+        user.setUsername("firstname@lastname");
+        user.setStatus(UserStatus.ONLINE);
+        user.setId(1L);
+        user.setToken("1");
+        user.setRoomId(1L);
+        List<User> users = Collections.singletonList(user);
+        Mockito.when(userRepository.findByRoomId(Mockito.anyLong())).thenReturn(users);
+
+        List<Long> allPlayerIds = roomService.getAllPlayersIds(room.getId());
+
+        assertEquals(1L, allPlayerIds.get(0));
+    }
 
 }
